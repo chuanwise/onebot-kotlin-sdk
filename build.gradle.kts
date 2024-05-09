5a87ad03e6683244eeeeed22e69b7ced981ffc82
Chuanwise <chuanwise@qq.com>
2024-05-09 19:36:24 +0800

    refactor(io.serialization): change `JSONElement` to `PlainElement`, use `Jackson` instenad of `kotlinx.serialization` for dynamic decoding.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,6 @@
 
 plugins {
     kotlin("jvm") version "1.9.10"
-    kotlin("plugin.serialization") version "1.9.10"
 }
 
 group = "cn.chuanwise"
@@ -28,24 +27,28 @@ repositories {
 
 dependencies {
     val ktorVersion = "2.3.10"
-    implementation("io.ktor:ktor-client-websockets:${ktorVersion}")
-//    implementation("io.ktor:ktor-client-cio:${ktorVersion}")
-//    implementation("io.ktor:ktor-client-js:${ktorVersion}")
-    implementation("io.ktor:ktor-client-okhttp:${ktorVersion}")
+    implementation("io.ktor:ktor-websockets:$ktorVersion")
+    implementation("io.ktor:ktor-client-websockets:$ktorVersion")
+//    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+//    implementation("io.ktor:ktor-client-js:$ktorVersion")
+    implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
 
     val log4jVersion = "2.23.1"
     val kotlinLoggingVersion = "5.1.0"
     val slf4jVersion = "2.0.13"
-    implementation("io.github.oshai:kotlin-logging-jvm:${kotlinLoggingVersion}")
-    implementation("org.apache.logging.log4j:log4j-api:${log4jVersion}")
-    runtimeOnly("org.apache.logging.log4j:log4j-core:${log4jVersion}")
-    testRuntimeOnly("org.apache.logging.log4j:log4j-core:${log4jVersion}")
+    implementation("io.github.oshai:kotlin-logging-jvm:$kotlinLoggingVersion")
+    implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
+    runtimeOnly("org.apache.logging.log4j:log4j-core:$log4jVersion")
+    testRuntimeOnly("org.apache.logging.log4j:log4j-core:$log4jVersion")
 
-    implementation("org.slf4j:slf4j-api:${slf4jVersion}")
-    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:${log4jVersion}")
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:$log4jVersion")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
+    val jacksonVersion = "2.17.1"
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
